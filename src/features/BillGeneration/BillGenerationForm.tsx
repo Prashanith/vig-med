fc486085e5eb6a117ac98999499c7d77e24cfa67
Prashanith <prashanith007@gmail.com>
2025-04-05 16:17:24 +0530

    added-packs

--- src/features/BillGeneration/BillGenerationForm.tsx
+++ src/features/BillGeneration/BillGenerationForm.tsx
@@ -2,13 +2,14 @@ import { useForm, useFieldArray } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { BillSchema } from "./types/bill";
 import { Product } from "./types/product";
-// import { PDFViewer } from "@react-pdf/renderer";
-// import BillPdfView from "./components/BillPdfView";
+import { PDFViewer } from "@react-pdf/renderer";
+import BillPdfView from "./components/BillPdfView";
 
 const BillGenerationForm = () => {
   const {
     register,
     handleSubmit,
+    getValues,
     control,
     formState: { errors },
   } = useForm({
@@ -294,9 +295,9 @@ const BillGenerationForm = () => {
           </button>
         </div>
 
-        {/* <PDFViewer>
+        <PDFViewer>
           <BillPdfView bill={getValues()} />
-        </PDFViewer> */}
+        </PDFViewer>
       </form>
     </div>
   );
