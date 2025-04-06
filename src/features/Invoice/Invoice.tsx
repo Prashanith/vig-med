bd86507c1e083008c7ce72c8be011a3144ce973b
Prashanith <prashanith007@gmail.com>
2025-04-06 20:00:11 +0530

    DEP

--- src/features/Invoice/Invoice.tsx
+++ src/features/Invoice/Invoice.tsx
@@ -6,9 +6,12 @@ import { PDFViewer } from "@react-pdf/renderer";
 import InvoiceViewer from "./components/InvoiceViewer";
 import ProductTable from "./components/ProductTable";
 import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
+import { FaFileInvoice, FaPlus } from "react-icons/fa6";
 
 const Invoice = () => {
   const [currentIndex, setCurrentIndex] = useState(-1);
+  const [previewInvoice, setPreviewInvoice] = useState(false);
   const {
     register,
     handleSubmit,
@@ -22,23 +25,7 @@ const Invoice = () => {
       name: "",
       email: "",
       totalAmount: 0,
-      products: [
-        {
-          id: 1,
-          name: "",
-          hsn: "",
-          batchNumber: "",
-          expiry: "",
-          mrp: 0,
-          quantity: 1,
-          freeQuantity: 0,
-          rate: 0,
-          amount: 0,
-          discount: 0,
-          cgst: 0,
-          sgst: 0,
-        },
-      ],
+      products: [],
     },
   });
 
@@ -67,6 +54,7 @@ const Invoice = () => {
     });
     setValue("products", products);
     setValue("totalAmount", totalBill);
+    setPreviewInvoice((prev) => true);
   };
 
   return (
@@ -74,9 +62,13 @@ const Invoice = () => {
       <h3 className='text-left justify-self-start text-3xl py-2 font-bold'>
         Invoice
       </h3>
+      <div className='w-full border-b-teal-700 border-[1px]'></div>
       <form onSubmit={handleSubmit(onSubmit)} className='w-full space-y-6'>
-        <div className='flex flex-row justify-start items-start space-x-4'>
-          <div className='flex flex-col justify-start items-start'>
+        <div className='flex flex-row justify-start items-start space-x-6'>
+          <div className='flex flex-col justify-start items-start space-y-6'>
+            <div className='w-full flex justify-between items-center'>
+              <h3 className='font-bold text-xl'>Invoice Information</h3>
+            </div>
             {/* Name Field */}
             <div className='flex flex-col'>
               <label htmlFor='name' className='text-lg'>
@@ -113,12 +105,22 @@ const Invoice = () => {
           </div>
           <div className='flex flex-col justify-start items-start space-y-4'>
             <div className='w-full flex justify-between items-center'>
-              <h3>Product List</h3>
-              <div className='flex justify-center'>
+              <h3 className='font-bold text-xl'>Product List</h3>
+              <div className='flex flex-row justify-center items-center space-x-3'>
+                {getValues() != null && getValues().products.length > 0 && (
+                  <div className='flex justify-center'>
+                    <button
+                      type='submit'
+                      className='bg-secondary text-primary-text py-2 px-4 rounded-md  flex justify-center items-center space-x-2'
+                    >
+                      <FaFileInvoice />
+                      <span>Generate Invoice</span>
+                    </button>
+                  </div>
+                )}
                 <button
                   type='button'
                   onClick={() => {
-                    // setCurrentIndex(fields.length + 1);
                     append({
                       id: fields.length + 1,
                       name: "",
@@ -134,10 +136,11 @@ const Invoice = () => {
                       cgst: 0,
                       sgst: 0,
                     });
+                    setCurrentIndex(fields.length);
                   }}
-                  className='bg-blue-500 text-white py-2 px-4 rounded-md'
+                  className='bg-primary text-white py-2 px-4 rounded-md flex justify-center items-center space-x-2'
                 >
-                  Add Product
+                  <FaPlus /> <span>Add Product</span>
                 </button>
               </div>
             </div>
@@ -151,152 +154,155 @@ const Invoice = () => {
 
         {currentIndex != -1 && (
           <div
-            className={`fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center transition-opacity duration-300 ${
-              currentIndex != -1
-                ? "opacity-100 pointer-events-auto"
-                : "opacity-0 pointer-events-none"
-            }`}
+            className={`flex justify-center items-center fixed z-20 top-0 left-0 w-full h-screen border border-gray-300 rounded-lg shadow-lg p-4 transition-transform duration-300 `}
           >
-            <h3 className='text-xl mb-3'>Product {currentIndex + 1}</h3>
-            <div className='grid grid-cols-2 gap-4'>
-              <div className='flex flex-col'>
-                <label>Product Name</label>
-                <input
-                  type='text'
-                  {...register(`products.${currentIndex}.name`)}
-                  placeholder='Name'
-                  className='inputField'
-                />
-                {errors.products?.[currentIndex]?.name && (
-                  <span className='text-red-500 mt-2'>
-                    {errors.products[currentIndex].name?.message}
+            <div className='w-[550px] rounded-lg bg-white shadow-2xl p-6'>
+              <div className='w-full flex justify-between items-start'>
+                <h3 className='text-md mb-8 font-bold'>
+                  Add/Modify Product {currentIndex + 1}{" "}
+                  <span className='text-sm text-gray-700 font-light italic'>
+                    (Information is autosaved)
                   </span>
-                )}
+                </h3>
+                <FaTimes
+                  className='cursor-pointer'
+                  onClick={() => setCurrentIndex(-1)}
+                />
+              </div>
+              <div className='grid grid-cols-2 gap-4'>
+                <div className='flex flex-col'>
+                  <label>Product Name</label>
+                  <input
+                    type='text'
+                    {...register(`products.${currentIndex}.name`)}
+                    placeholder='Name'
+                    className='inputField'
+                  />
+                  {errors.products?.[currentIndex]?.name && (
+                    <span className='text-red-500 mt-2'>
+                      {errors.products[currentIndex].name?.message}
+                    </span>
+                  )}
+                </div>
+
+                <div className='flex flex-col'>
+                  <label>HSN</label>
+                  <input
+                    type='text'
+                    {...register(`products.${currentIndex}.hsn`)}
+                    placeholder='HSN'
+                    className='inputField'
+                  />
+                </div>
               </div>
 
-              <div className='flex flex-col'>
-                <label>HSN</label>
-                <input
-                  type='text'
-                  {...register(`products.${currentIndex}.hsn`)}
-                  placeholder='HSN'
-                  className='inputField'
-                />
-              </div>
-            </div>
+              <div className='grid grid-cols-2 gap-4 mt-4'>
+                <div className='flex flex-col'>
+                  <label>Batch Number</label>
+                  <input
+                    type='text'
+                    {...register(`products.${currentIndex}.batchNumber`)}
+                    placeholder='Batch Number'
+                    className='inputField'
+                  />
+                </div>
 
-            <div className='grid grid-cols-2 gap-4 mt-4'>
-              <div className='flex flex-col'>
-                <label>Batch Number</label>
-                <input
-                  type='text'
-                  {...register(`products.${currentIndex}.batchNumber`)}
-                  placeholder='Batch Number'
-                  className='inputField'
-                />
-              </div>
-
-              <div className='flex flex-col'>
-                <label>Expiry Date</label>
-                <input
-                  type='text'
-                  {...register(`products.${currentIndex}.expiry`)}
-                  placeholder='Expiry'
-                  className='inputField'
-                />
-              </div>
-            </div>
-
-            <div className='grid grid-cols-2 gap-4 mt-4'>
-              <div className='flex flex-col'>
-                <label>MRP</label>
-                <input
-                  type='number'
-                  {...register(`products.${currentIndex}.mrp`, {
-                    valueAsNumber: true,
-                  })}
-                  placeholder='MRP'
-                  className='inputField'
-                />
+                <div className='flex flex-col'>
+                  <label>Expiry Date</label>
+                  <input
+                    type='text'
+                    {...register(`products.${currentIndex}.expiry`)}
+                    placeholder='Expiry'
+                    className='inputField'
+                  />
+                </div>
               </div>
 
-              <div className='flex flex-col'>
-                <label>Quantity</label>
-                <input
-                  type='text'
-                  {...register(`products.${currentIndex}.quantity`)}
-                  placeholder='Quantity'
-                  className='inputField'
-                />
-              </div>
-            </div>
+              <div className='grid grid-cols-2 gap-4 mt-4'>
+                <div className='flex flex-col'>
+                  <label>MRP</label>
+                  <input
+                    type='number'
+                    {...register(`products.${currentIndex}.mrp`, {
+                      valueAsNumber: true,
+                    })}
+                    placeholder='MRP'
+                    className='inputField'
+                  />
+                </div>
 
-            <div className='grid grid-cols-2 gap-4 mt-4'>
-              <div className='flex flex-col'>
-                <label>Free Quantity</label>
-                <input
-                  type='number'
-                  {...register(`products.${currentIndex}.freeQuantity`, {
-                    valueAsNumber: true,
-                  })}
-                  placeholder='Free Quantity'
-                  className='inputField'
-                />
+                <div className='flex flex-col'>
+                  <label>Quantity</label>
+                  <input
+                    type='text'
+                    {...register(`products.${currentIndex}.quantity`)}
+                    placeholder='Quantity'
+                    className='inputField'
+                  />
+                </div>
               </div>
 
-              <div className='flex flex-col'>
-                <label>Discount</label>
-                <input
-                  type='number'
-                  {...register(`products.${currentIndex}.discount`, {
-                    valueAsNumber: true,
-                  })}
-                  placeholder='Discount'
-                  className='inputField'
-                />
-              </div>
-            </div>
+              <div className='grid grid-cols-2 gap-4 mt-4'>
+                <div className='flex flex-col'>
+                  <label>Free Quantity</label>
+                  <input
+                    type='number'
+                    {...register(`products.${currentIndex}.freeQuantity`, {
+                      valueAsNumber: true,
+                    })}
+                    placeholder='Free Quantity'
+                    className='inputField'
+                  />
+                </div>
 
-            <div className='grid grid-cols-2 gap-4 mt-4'>
-              <div className='flex flex-col'>
-                <label>CGST</label>
-                <input
-                  type='number'
-                  {...register(`products.${currentIndex}.cgst`, {
-                    valueAsNumber: true,
-                  })}
-                  placeholder='CGST'
-                  className='inputField'
-                />
+                <div className='flex flex-col'>
+                  <label>Discount</label>
+                  <input
+                    type='number'
+                    {...register(`products.${currentIndex}.discount`, {
+                      valueAsNumber: true,
+                    })}
+                    placeholder='Discount'
+                    className='inputField'
+                  />
+                </div>
               </div>
-              <div className='flex flex-col'>
-                <label>SGST</label>
-                <input
-                  type='number'
-                  {...register(`products.${currentIndex}.sgst`, {
-                    valueAsNumber: true,
-                  })}
-                  placeholder='SGST'
-                  className='inputField'
-                />
+
+              <div className='grid grid-cols-2 gap-4 mt-4'>
+                <div className='flex flex-col'>
+                  <label>CGST</label>
+                  <input
+                    type='number'
+                    {...register(`products.${currentIndex}.cgst`, {
+                      valueAsNumber: true,
+                    })}
+                    placeholder='CGST'
+                    className='inputField'
+                  />
+                </div>
+                <div className='flex flex-col'>
+                  <label>SGST</label>
+                  <input
+                    type='number'
+                    {...register(`products.${currentIndex}.sgst`, {
+                      valueAsNumber: true,
+                    })}
+                    placeholder='SGST'
+                    className='inputField'
+                  />
+                </div>
               </div>
             </div>
           </div>
         )}
-        <div className='flex justify-center'>
-          <button
-            type='submit'
-            className='bg-green-500 text-white py-2 px-4 rounded-md mt-6'
-          >
-            Generate Invoice
-          </button>
-        </div>
       </form>
-      {getValues() != null && getValues().products.length > 0 && (
-        <PDFViewer className='w-full h-screen'>
-          <InvoiceViewer invoice={getValues()} />
-        </PDFViewer>
-      )}
+      {getValues() != null &&
+        getValues().products.length > 0 &&
+        previewInvoice && (
+          <PDFViewer className='w-full h-screen'>
+            <InvoiceViewer invoice={getValues()} />
+          </PDFViewer>
+        )}
     </div>
   );
 };
