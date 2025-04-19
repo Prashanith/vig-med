ab480f390d62cfc06224934d053f3d21a1583fa9
Prashanith <prashanith007@gmail.com>
2025-04-19 23:51:31 +0530

    fix

--- src/components/scaffold/AppScaffold.tsx
+++ src/components/scaffold/AppScaffold.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router";
+import Button from "../button/button";
+import { FaSignOutAlt } from "react-icons/fa";
+import { logOut } from "../../features/authentication/services/authService";
 
 function AppScaffold() {
   return (
     <div className='w-full h-screen flex flex-col justify-start items-start'>
-      <nav className='pagePadding h-[70px] w-full flex justify-start items-center bg-primary text-secondary-text'>
+      <nav className='pagePadding h-[70px] w-full flex justify-between items-center bg-primary text-secondary-text'>
         <h2 className='text-lg font-medium'>
           <img src='/logo.svg' className='h-12' />
         </h2>
+        <Button onClick={() => logOut()} endIcon={<FaSignOutAlt />}>
+          SIGN OUT
+        </Button>
       </nav>
       <div className='pagePadding h-full overflow-y-scroll w-full p-6 splashBg'>
         <Outlet />
