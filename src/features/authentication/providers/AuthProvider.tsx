e640e75a0d54e5c33dba19f79b61c99232a254d4
Prashanith <prashanith007@gmail.com>
2025-04-27 14:02:40 +0530

    fix

--- src/features/authentication/providers/AuthProvider.tsx
+++ src/features/authentication/providers/AuthProvider.tsx
@@ -2,7 +2,7 @@ import { User } from "firebase/auth";
 import { ReactNode, useState, useEffect } from "react";
 import { auth } from "../../../services/firebase";
 import { AuthContext } from "../context/useAuth";
-import { useLocation, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 interface IAuthProvider {
   children: ReactNode;
@@ -11,17 +11,17 @@ interface IAuthProvider {
 export function AuthProvider({ children }: IAuthProvider) {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const navigate = useNavigate();
-  const location = useLocation();
+  // const location = useLocation();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
       setTimeout(() => {
-        if (user && location.pathname.includes("login")) {
-          navigate("/dashboard", { replace: true });
-        } else {
-          navigate("/login", { replace: true });
-        }
+        navigate("/dashboard", { replace: true });
+        // if (user && location.pathname.includes("login")) {
+        // } else {
+        //   navigate("/login", { replace: true });
+        // }
       }, 1000);
     });
     return unsubscribe;
